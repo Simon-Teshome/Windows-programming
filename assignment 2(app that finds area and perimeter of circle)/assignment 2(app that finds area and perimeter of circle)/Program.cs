558eb5d4c8e49415a1dc18ee6ea74d668dc67914
Simon-Teshome <101554448+Simon-Teshome@users.noreply.github.com>
2022-06-26 12:15:13 +0300

    Assignment that has if statements and iterations

---
+++ assignment 2(app that finds area and perimeter of circle)/assignment 2(app that finds area and perimeter of circle)/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int input = 1;
+double r, perimeter, area;
+
+while (true)
+{
+    Console.Write("Please write the radius of your circle : ");
+    r = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("1.To find PERIMETER of a Circle\n2.To find AREA of a circle");
+    input = int.Parse(Console.ReadLine());
+    if(input == 1)
+    {
+        perimeter = 2 * 3.14 * r;
+        Console.WriteLine("=============================================");
+        Console.WriteLine("The perimeter of yor circle : " + perimeter);
+    }
+    else if(input == 2)
+    {
+        area = 3.14 * Math.Pow(r, 2);
+        Console.WriteLine("=============================================");
+        Console.WriteLine("The area of yor circle : " + area);
+    }
+
+    Console.WriteLine("Press any key to calculate again");
+    Console.ReadKey();
+    Console.WriteLine("\n\n\n");
+}
